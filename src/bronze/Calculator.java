d20ab57fec626162e9926845776038fb807e06d5
Elena Lee <tbfjs3976@icloud.com>
2025-09-29 22:21:40 +0900

    9/28

---
+++ src/bronze/Calculator.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}

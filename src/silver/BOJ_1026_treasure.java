e8c185733a85cf049c5469bcb5e60f3870e2d45a
Elena Lee <tbfjs3976@icloud.com>
2026-02-26 23:03:04 +0900

    02/26

---
+++ src/silver/BOJ_1026_treasure.java
@@ -0,0 +1,36 @@
+package silver;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BOJ_1026_treasure {
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+         int[] a = new int[n];
+         Integer[] b = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b,Comparator.reverseOrder());
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += a[i] * b[i];
+        }
+
+        System.out.println(total);
+
+    }
+
+}

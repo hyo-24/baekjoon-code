2af646561dd6cfa5ffd522e08539d3c55ce96cb1
Elena Lee <tbfjs3976@icloud.com>
2026-03-04 13:00:39 +0900

    03/04

--- src/silver/BOJ_2667_bowl.java
+++ src/silver/BOJ_2667_bowl.java
@@ -47,8 +47,8 @@ public class BOJ_2667_bowl {
     }
 
     static int dfs(int x, int y) {
-        check[x][y] = true;
-        int doors = 1;
+        check[x][y] = true; // 처음 들어와서 본인에 방문 확인!
+        int doors = 1; // 부지에 있는 집 개수에 나도 포함시키기
 
         int[] dx = {0,0,1,-1};
         int[] dy = {1,-1,0,0};
@@ -56,9 +56,13 @@ public class BOJ_2667_bowl {
         for (int i = 0; i < 4; i++) {
             int dirX = x + dx[i];
             int dirY = y +dy[i];
-            if (dirX >= 0 && dirX < door && dirY >= 0 && dirY < door) {
+            if (dirX >= 0 && dirX < door && dirY >= 0 && dirY < door) { // 조건 : 좌표를 잘 생각해보면 작성 가능
                 if (apart[dirX][dirY] == 1 && !check[dirX][dirY]) {
                     doors += dfs(dirX,dirY);
+                    /*  ✅콜 스택 (Call Stack) : += 연산은 dfs 함수가 완전히 끝나서 값을 들고 돌아온 직후에 실행된다.
+                    그래서 리턴값을 받기 전에는 계산되지 않고 기다리고 있다가, 마지막 차례가 1을 들고 오기 시작하면 그때부터
+                    도미노처럼 촤르륵 더해진다! 쌓여있다가 거꾸로 올라오면서 계산
+                     */
                 }
             }
         }

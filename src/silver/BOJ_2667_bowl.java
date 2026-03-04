06a744f8f95813214ae19898bea80884a42f3d51
Elena Lee <tbfjs3976@icloud.com>
2026-03-04 12:51:19 +0900

    03/04

--- src/silver/BOJ_2667_bowl.java
+++ src/silver/BOJ_2667_bowl.java
@@ -1,13 +1,67 @@
 package silver;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class BOJ_2667_bowl {
 
+ static int[][] apart;
+ static int apartNum;
+ static boolean[][] check;
+ static int door;
 
 
     static void main() {
         Scanner scanner = new Scanner(System.in);
 
+        door = scanner.nextInt();
+        apart = new int[door][door];
+        apartNum = 0;
+        check = new boolean[door][door];
+
+        for (int i = 0; i < door; i++) {
+            String line = scanner.next();
+            for (int j = 0; j < door; j++) {
+                apart[i][j] = line.charAt(j) -'0';
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < door; i++) {
+            for (int j = 0; j < door; j++) {
+                if (apart[i][j] == 1 && !check[i][j]) {
+                    int doors = dfs(i, j);
+                    result.add(doors);
+                    apartNum++;
+                }
+            }
+        }
+        System.out.println(apartNum);
+        Collections.sort(result);
+        for (Integer i : result) {
+            System.out.println(i);
+        }
+
+
+    }
+
+    static int dfs(int x, int y) {
+        check[x][y] = true;
+        int doors = 1;
+
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+
+        for (int i = 0; i < 4; i++) {
+            int dirX = x + dx[i];
+            int dirY = y +dy[i];
+            if (dirX >= 0 && dirX < door && dirY >= 0 && dirY < door) {
+                if (apart[dirX][dirY] == 1 && !check[dirX][dirY]) {
+                    doors += dfs(dirX,dirY);
+                }
+            }
+        }
+        return doors;
     }
 }

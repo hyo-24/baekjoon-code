62537aae665de79ba2d6f91b1d4a75668e98f806
Elena Lee <tbfjs3976@icloud.com>
2026-03-12 16:47:58 +0900

    03/12

---
+++ src/silver/review/BOJ_2667_Bowl_Review.java
@@ -0,0 +1,12 @@
+package silver.review;
+
+import java.util.Scanner;
+
+public class BOJ_2667_Bowl_Review {
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
